a8c156fc7b1e6e02caa14ebd8fbfe0d77f42b41c
Shyrokyi Vladislav <vlad16062001@gmail.com>
2021-04-22 20:13:25 +0300

    added system file

---
+++ ConsoleOut/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+using Core_2048;
+
+namespace ConsoleOut
+{
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new Core(4, 4, 0, 0,
+                (value, oldValue) => value * 2
+                );
+            app.SetValue(new Element(2, 2, app.StandardNewValue));
+            Render(app);
+            while (true)
+            {
+                var direction = Input();
+                if (direction != null)
+                {
+                    var isAlongRow = direction is Direction.Left or Direction.Right;
+                    var isIncreasing = direction is Direction.Left or Direction.Up;
+                    app.UpdateElements(isAlongRow, isIncreasing);
+                }
+
+                if (app.HasUpdated)
+                {
+                    var newRandom = app.NewElement();
+                    if (newRandom != null)
+                    {
+                        app.SetValue(newRandom);
+                    }
+                }
+                Render(app);
+            }
+        }
+
+        private static void Render(Core app)
+        {
+            Console.Clear();
+            var prevRow = -1;
+            var pattern = new Func<int, string>(value => $"  {value}  |");
+            app.ForEach(element =>
+            {
+                var cell = pattern(element.Value);
+                if (prevRow == element.Row)
+                {
+                    Console.Write(cell);
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    Console.Write($"|{cell}");
+                    prevRow = element.Row;
+                }
+            });
+        }
+
+        private static Direction? Input()
+        {
+            var key = Console.ReadKey();
+
+            return key.Key switch
+            {
+                ConsoleKey.W => Direction.Up,
+                ConsoleKey.S => Direction.Down,
+                ConsoleKey.A => Direction.Left,
+                ConsoleKey.D => Direction.Right,
+                _            => null
+            };
+        }
+    }
+
+    internal enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Core_2048")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Core_2048")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("FF86D050-B76D-4D93-80A0-22BA15BBDD90")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
