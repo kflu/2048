117265c96eb6c5f43fbef2ce6008035b28c29ade
Shyrokyi Vladislav <vlad16062001@gmail.com>
2021-07-10 17:55:22 +0300

    [ADD] Comments to Element.cs

--- C#/Element.cs
+++ C#/Element.cs
@@ -3,10 +3,24 @@
 
     public partial class Core<T>
     {
+        /// <summary>
+        ///     Container for value and indexes in inner/outer dimensions
+        /// </summary>
         public class Element
         {
+            /// <summary>
+            ///     Index in inner array dimensions
+            /// </summary>
             public int Column;
+
+            /// <summary>
+            ///     Index in outer array dimensions
+            /// </summary>
             public int Row;
+
+            /// <summary>
+            ///     Value on array
+            /// </summary>
             public T Value;
         }
     }
