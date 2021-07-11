a505d43a0075b4a542a4caf9533bf3a2c0aa5880
Shyrokyi Vladislav <vlad16062001@gmail.com>
2021-07-11 19:59:54 +0300

    [FIX] Removed redundant references, changed nuget package reference to local project solution references in example

--- C#/IElementGenerator.cs
+++ C#/IElementGenerator.cs
@@ -1,11 +1,10 @@
-﻿using JetBrains.Annotations;
-
+﻿
 namespace Core_2048
 {
 
     public interface IElementGenerator<T>
     {
-        [CanBeNull] Element<T> GetNewElement(Board<T> board);
+        Element<T> GetNewElement(Board<T> board);
     }
 
 }
